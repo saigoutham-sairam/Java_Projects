81ea0feae1fea6b2c5af2aac086c210ddc927812
Saigoutham Sairam <sairam.saigoutham@gmail.com>
2025-04-18 01:41:09 +0530

    Update VirtualThreadService.java

--- concurrent-task-processing/src/main/java/com/example/multithreading/service/VirtualThreadService.java
+++ concurrent-task-processing/src/main/java/com/example/multithreading/service/VirtualThreadService.java
@@ -2,15 +2,21 @@ package com.example.multithreading.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
 
 @Service
-public
-class VirtualThreadService {
+public class VirtualThreadService {
 
+    public void executeTasks() {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < 100; i++) {
+                executor.submit(() -> {
+                    TaskSimulator.simulateTask();
+                    System.out.println("Task executed by virtual thread: " + Thread.currentThread().getName());
+                });
+            }
+        }
+    }
 }
+
 
