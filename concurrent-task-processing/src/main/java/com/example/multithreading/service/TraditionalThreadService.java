3fe5063901d7c255733dcc3175033c641cfa2c4a
Saigoutham Sairam <sairam.saigoutham@gmail.com>
2025-04-18 01:40:37 +0530

    Update TraditionalThreadService.java

--- concurrent-task-processing/src/main/java/com/example/multithreading/service/TraditionalThreadService.java
+++ concurrent-task-processing/src/main/java/com/example/multithreading/service/TraditionalThreadService.java
@@ -1,16 +1,26 @@
 package com.example.multithreading.service;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
-public
-class TraditionalThreadService {
+public class TraditionalThreadService {
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    @Async
+    public void executeTasks() {
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(() -> {
+                TaskSimulator.simulateTask();
+                System.out.println("Task executed by traditional thread: " + Thread.currentThread().getName());
+            });
+        }
+        executorService.shutdown();
+    }
 }
+
 
